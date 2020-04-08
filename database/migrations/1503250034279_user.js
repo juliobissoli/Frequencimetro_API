474c53f5e16c52ebe0c3b7d65fc4d7e17cf97b08
Julio <juliobissoli33@gmail.com>
2020-04-08 12:42:50 -0300

    Remove coluns exedent in user table

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -11,10 +11,6 @@ class UserSchema extends Schema {
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
       table.string('type', 20),
-      table.string('address', 254),
-      table.string('cpf', 20)
-      table.string('schedules', 255),
-      table.decimal('payment')
       table.timestamps()
     })
   }
